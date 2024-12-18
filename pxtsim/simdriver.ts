a562711866afc00b269166a0c72c89dd80e5f567
Danish <danish@reckonsys.com>
2024-12-18 14:55:55 +0530

    updated furl

--- pxtsim/simdriver.ts
+++ pxtsim/simdriver.ts
@@ -287,22 +287,12 @@ namespace pxsim {
         }
 
         private getSimUrl(): URL {
-            console.log(this.options,'....this.options');
-            console.log(((window as any).pxtConfig || {}).simUrl,'.....simUrl');
-            console.log(location,'...location.origin');
-            // http://pxt.makewonder.com/pxt/1e96d4f1be82dcaa6f5af79f1949ee9af191525c/index.html#editor
-            let lastSlashIndex = location.href.lastIndexOf('/');
-            let baseUrl = location.href.substring(0, lastSlashIndex);
-            console.log(baseUrl,'...baseUrl');
-            const simUrl = this.options.simUrl || ((window as any).pxtConfig || {}).simUrl || `${baseUrl}/sim/simulator.html`;
-            console.log(simUrl,'...simUrl');
+            const simUrl = this.options.simUrl || ((window as any).pxtConfig || {}).simUrl || `${location.origin}/sim/simulator.html`;
             try {
-                console.log('try..');
                 return new URL(simUrl);
             } catch {
-                console.log('catch..');
                 // Failed to parse set url; try based off origin in case path defined as relative (e.g. /simulator.html)
-                return new URL(simUrl, baseUrl);
+                return new URL(simUrl, location.origin);
             }
         }
 
@@ -426,10 +416,9 @@ namespace pxsim {
             frame.setAttribute('allow', 'autoplay');
             frame.setAttribute('sandbox', 'allow-same-origin allow-scripts');
             frame.className = 'no-select'
-            const furl = (url || this.getSimUrl()) + '#' + frame.id;
-            console.log(furl,'...furl');
-            console.log(url,'....url');
-            console.log(this.getSimUrl(),'....getSimUrl()');
+            let lastSlashIndex = location.href.lastIndexOf('/');
+            let baseUrl = location.href.substring(0, lastSlashIndex);
+            const furl = baseUrl + '/simulator.html#' + frame.id;
             frame.src = furl;
             frame.frameBorder = "0";
             frame.dataset['runid'] = this.runId;
