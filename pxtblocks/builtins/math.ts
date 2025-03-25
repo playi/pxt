0d5e0484acacc581fd67ce0b713ff7b47430133d
Danish <danish@reckonsys.com>
2025-03-25 12:43:18 +0530

    ts error fix

--- pxtblocks/builtins/math.ts
+++ pxtblocks/builtins/math.ts
@@ -159,7 +159,7 @@ export function initMathOpBlock() {
             // Because the shape of inputs changes, we need a mutation. Technically the op tells us
             // how many inputs we should have but we can't read its value at init time
             appendMutation(b, {
-                mutationToDom: mutation => {
+                mutationToDom: (mutation: Element) => {
                     let infix: boolean;
                     for (let i = 0; i < b.inputList.length; i++) {
                         const input = b.inputList[i];
@@ -175,7 +175,7 @@ export function initMathOpBlock() {
                     mutation.setAttribute("op-type", (b.getInput("ARG1") ? (infix ? "infix" : "binary") : "unary").toString());
                     return mutation;
                 },
-                domToMutation: saved => {
+                domToMutation: (saved: Element) => {
                     if (saved.hasAttribute("op-type")) {
                         const type = saved.getAttribute("op-type");
                         if (type != "unary") {
